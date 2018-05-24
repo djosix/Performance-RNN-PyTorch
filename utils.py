627559b7307d1c6b42507e7fb92a381290de56e3
djosix <djosicks@gmail.com>
2018-05-25 01:10:34 +0800

    Remove unused function

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import os
+import numpy as np
+from sequence import EventSeq, ControlSeq
+
 
 def find_files_by_extensions(root, exts=[]):
     def _has_ext(name):
@@ -8,14 +12,12 @@ def find_files_by_extensions(root, exts=[]):
             if name.endswith(ext):
                 return True
         return False
-    import os
     for path, _, files in os.walk(root):
         for name in files:
             if _has_ext(name):
                 yield os.path.join(path, name)
 
 def event_indeces_to_midi_file(event_indeces, midi_file_name, velocity_scale=0.8):
-    from sequence import EventSeq
     event_seq = EventSeq.from_array(event_indeces)
     note_seq = event_seq.to_note_seq()
     for note in note_seq.notes:
@@ -23,20 +25,9 @@ def event_indeces_to_midi_file(event_indeces, midi_file_name, velocity_scale=0.8
     note_seq.to_midi_file(midi_file_name)
     return len(note_seq.notes)
 
-def make_controls(batch_size, pitch_probs, density, steps=1):
-    from sequence import Control, ControlSeq
-    import numpy as np
-    assert pitch_probs.shape[0] == 12
-    assert density in range(0, ControlSeq.feat_dims()['note_density'])
-    control = Control(pitch_probs, density).to_array()
-    controls = np.array([[control] * batch_size] * steps)
-    return controls
-
 def transposition(events, controls, offset=0):
     # events [steps, batch_size, event_dim]
     # return events, controls
-    from sequence import EventSeq, ControlSeq
-    import numpy as np
 
     events = np.array(events, dtype=np.int64)
     controls = np.array(controls, dtype=np.float32)
