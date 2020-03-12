a9d577cec0cf45430b3a75231a1c5c0dff8fe1ef
djosix <djosicks@gmail.com>
2020-03-12 20:28:32 +0800

    Add process pool executor

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ import sys
 import torch
 import hashlib
 from progress.bar import Bar
+from concurrent.futures import ProcessPoolExecutor
 
 from sequence import NoteSeq, EventSeq, ControlSeq
 import utils
@@ -16,31 +17,35 @@ def preprocess_midi(path):
     control_seq = ControlSeq.from_event_seq(event_seq)
     return event_seq.to_array(), control_seq.to_compressed_array()
 
-def preprocess_midi_files_under(midi_root, save_dir):
+def preprocess_midi_files_under(midi_root, save_dir, num_workers):
     midi_paths = list(utils.find_files_by_extensions(midi_root, ['.mid', '.midi']))
     os.makedirs(save_dir, exist_ok=True)
     out_fmt = '{}-{}.data'
+    
+    results = []
+    executor = ProcessPoolExecutor(num_workers)
 
-    for path in Bar('Processing').iter(midi_paths):
-        print(' ', end='[{}]'.format(path), flush=True)
-
+    for path in midi_paths:
         try:
-            data = preprocess_midi(path)
+            results.append((path, executor.submit(preprocess_midi, path)))
         except KeyboardInterrupt:
             print(' Abort')
             return
         except:
             print(' Error')
             continue
-
+    
+    for path, future in Bar('Processing').iter(results):
+        print(' ', end='[{}]'.format(path), flush=True)
         name = os.path.basename(path)
         code = hashlib.md5(path.encode()).hexdigest()
         save_path = os.path.join(save_dir, out_fmt.format(name, code))
-        torch.save(data, save_path)
+        torch.save(future.result(), save_path)
 
     print('Done')
 
 if __name__ == '__main__':
     preprocess_midi_files_under(
             midi_root=sys.argv[1],
-            save_dir=sys.argv[2])
+            save_dir=sys.argv[2],
+            num_workers=int(sys.argv[3]))
